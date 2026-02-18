2a987134c6bf3352f816248902f9bd17614d1971
Gurrnos <gurrnos01@gmail.com>
2026-02-18 13:34:28 +0100

    updated connection config

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -12,17 +12,18 @@ def connect_db():
             'user': os.getenv("DB_USER"),
             'host': os.getenv("DB_HOST"),
             'port': int(os.getenv("DB_PORT")),
-            'password': os.getenv("DB_PASSWORD")
+            'password': os.getenv("DB_PASSWORD"),
+            'database': os.getenv("DATABASE")
         }
 
 
         cnx = mysql.connector.connect(**config)
 
-        print(f"Connection made to user: {config['user']} on port: {config['port']}")
+        print(f"Connection made to user: {config['user']} on port: {config['port']} with db: {config['database']}")
     
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
+            print("Access denied error: Check your username and password to make sure they match")
 
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
             print("Database does not exist")
