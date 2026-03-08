e1ca03417e5b65d5ed79defe4203e376010e3e56
Gurrnos <gurrnos01@gmail.com>
2026-03-08 13:33:23 +0100

    can now add recipes from account page

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -13,6 +13,20 @@ const Account = () => {
     const [usernameData, setUsernameData] = useState({
         new_username: ""
     });
+    const [recipeData, setRecipeData] = useState({
+        recipename: "",
+        description: "",
+        ispublic: 0,
+        ingredients: [],
+        steps: []
+    });
+    const initialRecipeData = {
+        recipename: "",
+        description: "",
+        ispublic: 0,
+        ingredients: [],
+        steps: []
+    };
 
     useEffect(() => {
         auth();
@@ -115,6 +129,92 @@ const Account = () => {
         }
     }
 
+    // Handle changes for inputs (recipename, description, ispublic)
+    const handleRecipeChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setRecipeData((prevState) => ({
+            ...prevState,
+            [name]: type === 'checkbox' ? (checked ? 1 : 0) : value
+        }));
+    };
+
+    // Handle changes for individual ingredient fields
+    const updateIngredient = (index, field, value) => {
+        const updatedIngredients = [...recipeData.ingredients];
+        updatedIngredients[index][field] = value;
+        setRecipeData((prevState) => ({
+            ...prevState,
+            ingredients: updatedIngredients
+        }));
+    };
+
+    // Add a new ingredient form
+    const addIngredient = () => {
+        setRecipeData((prevState) => ({
+            ...prevState,
+            ingredients: [...prevState.ingredients, { name: "", type: "", amount: "" }]
+        }));
+    };
+
+    // Remove an ingredient
+    const removeIngredient = (index) => {
+        const updatedIngredients = recipeData.ingredients.filter((_, i) => i !== index);
+        setRecipeData((prevState) => ({
+            ...prevState,
+            ingredients: updatedIngredients
+        }));
+    };
+
+    // Handle changes for steps
+    const updateStep = (index, value) => {
+        const updatedSteps = [...recipeData.steps];
+        updatedSteps[index] = value;
+        setRecipeData((prevState) => ({
+            ...prevState,
+            steps: updatedSteps
+        }));
+    };
+
+    // Add a new step field
+    const addStep = () => {
+        setRecipeData((prevState) => ({
+            ...prevState,
+            steps: [...prevState.steps, ""]
+        }));
+    };
+
+    // Remove a step
+    const removeStep = (index) => {
+        const updatedSteps = recipeData.steps.filter((_, i) => i !== index);
+        setRecipeData((prevState) => ({
+            ...prevState,
+            steps: updatedSteps
+        }));
+    };
+
+     const resetForm = () => {
+        setRecipeData(initialRecipeData);
+    };
+
+    const createRecipe = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            ...recipeData
+        };
+
+        console.log(data);
+
+        try {
+            await Axios.post("/api/createRecipe", data, { withCredentials: true });
+
+            alert("Successfully created recipe");
+            resetForm();
+        } catch (err) {
+            alert(err.response?.data?.message);
+        }
+    }
+
     return(
         <>
             <h1>Account page</h1>
@@ -122,6 +222,7 @@ const Account = () => {
             <h2>Hello {username}</h2>
 
             <h2>Your favorited recepies:</h2>
+            <div className="recipes-grid">
             {favoritedRecipes.map((recipe) => (
                 <RecipeDisplay
                     key={recipe.rid}
@@ -130,16 +231,98 @@ const Account = () => {
                     description={recipe.description}
                 />
             ))}
+            </div>
 
             <h2>Your recipes:</h2>
-            {yourRecipes.map((recipe) => (
-                <RecipeDisplay
-                    key={recipe.rid}
-                    rid={recipe.rid}
-                    name={recipe.name}
-                    description={recipe.description}
+            <div className="recipes-grid">
+                {yourRecipes.map((recipe) => (
+                    <RecipeDisplay
+                        key={recipe.rid}
+                        rid={recipe.rid}
+                        name={recipe.name}
+                        description={recipe.description}
+                    />
+                ))}
+            </div>
+            <h2>Create new recipe:</h2>
+            <form onSubmit={createRecipe}>
+                <div>
+                <label htmlFor="recipename">Recipe Name:</label>
+                <input
+                    type="text"
+                    id="recipename"
+                    name="recipename"
+                    value={recipeData.recipename}
+                    onChange={handleRecipeChange}
                 />
-            ))}
+            </div>
+            <div>
+                <label htmlFor="description">Description:</label>
+                <textarea
+                    id="description"
+                    name="description"
+                    value={recipeData.description}
+                    onChange={handleRecipeChange}
+                ></textarea>
+            </div>
+            <div>
+                <label htmlFor="ispublic">Is Public:</label>
+                <input
+                    type="checkbox"
+                    id="ispublic"
+                    name="ispublic"
+                    checked={recipeData.ispublic}
+                    onChange={handleRecipeChange}
+                />
+            </div>
+
+            <div>
+                <h3>Ingredients:</h3>
+                <button type="button" onClick={addIngredient}>Add Ingredient</button>
+                {recipeData.ingredients.map((ingredient, index) => (
+                    <div key={index}>
+                        <label>Ingredient Name:</label>
+                        <input
+                            type="text"
+                            value={ingredient.name}
+                            onChange={(e) => updateIngredient(index, 'name', e.target.value)}
+                        />
+                        <label>Ingredient Type:</label>
+                        <input
+                            type="text"
+                            value={ingredient.type}
+                            onChange={(e) => updateIngredient(index, 'type', e.target.value)}
+                        />
+                        <label>Amount:</label>
+                        <input
+                            type="text"
+                            value={ingredient.amount}
+                            onChange={(e) => updateIngredient(index, 'amount', e.target.value)}
+                        />
+                        <button type="button" onClick={() => removeIngredient(index)}>Remove Ingredient</button>
+                    </div>
+                ))}
+            </div>
+
+            <div>
+                <h3>Steps:</h3>
+                <button type="button" onClick={addStep}>Add Step</button>
+                {recipeData.steps.map((step, index) => (
+                    <div key={index}>
+                        <label>Step {index + 1}:</label>
+                        <input
+                            type="text"
+                            value={step}
+                            onChange={(e) => updateStep(index, e.target.value)}
+                        />
+                        <button type="button" onClick={() => removeStep(index)}>Remove Step</button>
+                    </div>
+                ))}
+            </div>
+
+            <button type="submit">Submit</button>
+            </form>
+
             <h2>Change password</h2>
             <form onSubmit={(e) => updateData(e, "passw")}>
                 <label>Old password:</label>
